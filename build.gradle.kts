7f38e71ce1f7a0eb1b3fbcc74a160f7b7b22d7d1
mon0mon <mon0mon@naver.com>
2024-11-30 12:30:41 +0900

    🎉 init : 신규 프로젝트 시작

---
+++ build.gradle.kts
@@ -0,0 +1,55 @@
+plugins {
+    java
+    id("org.springframework.boot") version "3.4.0"
+    id("io.spring.dependency-management") version "1.1.6"
+}
+
+group = "com.mon0mon"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+configurations {
+    compileOnly {
+        extendsFrom(configurations.annotationProcessor.get())
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly("org.projectlombok:lombok")
+
+    runtimeOnly("com.h2database:h2")
+
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    developmentOnly("org.springframework.boot:spring-boot-devtools")
+
+    annotationProcessor("org.springframework.boot:spring-boot-configuration-processor")
+    annotationProcessor("org.projectlombok:lombok")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    // https://mvnrepository.com/artifact/org.mockito/mockito-junit-jupiter
+    testImplementation("org.mockito:mockito-junit-jupiter:5.14.2")
+    // https://mvnrepository.com/artifact/com.tngtech.archunit/archunit
+    testImplementation("com.tngtech.archunit:archunit:1.3.0")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
+
+tasks.withType(JavaCompile::class) {
+    options.encoding = "UTF-8"
+}
---
+++ src/main/java/com/mon0mon/makingcleanarchitecture/MakingCleanArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.mon0mon.makingcleanarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakingCleanArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MakingCleanArchitectureApplication.class, args);
+    }
+
+}
